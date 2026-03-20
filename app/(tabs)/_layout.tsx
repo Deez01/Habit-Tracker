8c58bda0a449a885739cd5424730f23a886d8c8b
Deez01 <149739324+Deez01@users.noreply.github.com>
2026-03-19 22:23:46 -0700

    Added Icons for the app.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,6 +5,11 @@ import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import Octicons from '@expo/vector-icons/Octicons';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import Entypo from '@expo/vector-icons/Entypo';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -20,14 +25,35 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => <Octicons name="home-fill" size={28} color="black" />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="habbits"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Habbits',
+          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="heart-plus" size={28} color="black" />,
+        }}
+      />
+      <Tabs.Screen
+        name="streak"
+        options={{
+          title: 'Streak',
+          tabBarIcon: ({ color }) => <Ionicons name="calendar-clear" size={28} color="black" />,
+        }}
+      />
+      <Tabs.Screen
+        name="customize"
+        options={{
+          title: 'Customize',
+          tabBarIcon: ({ color }) => <Entypo name="pencil" size={28} color="black" />,
+        }}
+      />
+      <Tabs.Screen
+        name="aboutpage"
+        options={{
+          title: 'About Page',
+          tabBarIcon: ({ color }) => <FontAwesome5 name="question" size={28} color="black" />,
         }}
       />
     </Tabs>
